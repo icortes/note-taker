250f066ac0b1bdf2c1efbcdec9d9fc0dcee86c65
isa <5445946+icortes@users.noreply.github.com>
2021-10-19 16:25:03 -0700

    added path for /notes

--- server.js
+++ server.js
@@ -12,5 +12,15 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// GET Route for notes
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// GET Route for anything else
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
