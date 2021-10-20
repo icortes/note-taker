ba08c008565e6d327adf5ee0820f0c7357e55d7a
isa <5445946+icortes@users.noreply.github.com>
2021-10-19 17:03:18 -0700

    added required middleware

--- server.js
+++ server.js
@@ -8,15 +8,17 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // GET Route for notes
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET Route for api/notes
@@ -28,9 +30,20 @@ app.get('/api/notes', (req, res) => {
     return res.json(notes);
 });
 
+//POST Route for api/notes
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+
+    // Prepare a response object to send back to the client
+    let response;
+    console.log(req.body);
+
+})
+
 // GET Route for anything else
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 
